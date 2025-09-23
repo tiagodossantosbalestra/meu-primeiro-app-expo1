720ddd02b7aa5cea436d22d0231b8b6eac704c20
Tiago <tiago.amancio@escola.pr.gov.br>
2025-09-23 11:19:13 +0000

    salvo

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import meunome from './componentes/primeirocomponente';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Tiago dos Santos Balestra</Text>
+      <meunome />
       <StatusBar style="auto" />
     </View>
   );
---
+++ componentes/primeirocomponente.js
@@ -0,0 +1,5 @@
+import react from 'react'
+
+export default function meunome(){
+    <h1>Tiago</h1>
+}
