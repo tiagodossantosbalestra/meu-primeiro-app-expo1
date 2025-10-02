2c9352e340a9ea5cd0e547d19e0ddbfab1fef85c
Tiago <tiago.amancio@escola.pr.gov.br>
2025-10-02 12:55:24 +0000

    13

---
+++ componentes/texto.js
@@ -0,0 +1,17 @@
+import react from "react";
+import { StyleSheet } from "react-native";
+
+export default function texto(children, style){
+    let estilo = estilos.texto;
+    if(style?.fontWheight ==='bold'){
+        estilo.estilos.texto negrito
+}
+
+return <text style={[style,estilo]}> {children} </text>
+}
+
+const estilos = stylesheet.create({
+    texto:{
+
+
+}
---
+++ componentes/topo.js
@@ -0,0 +1,8 @@
+import react from "react";
+import{ image,StyleSheet,dimensions} from "react native";
+
+export defult function topo(){
+    return<>
+
+    </>
+}
