704fbb9bbb83930d3b3a4e8f68227dbadb3e8fbb
Tiago <tiago.amancio@escola.pr.gov.br>
2025-10-02 13:57:46 +0000

    16

--- componentes/topo.js
+++ componentes/topo.js
@@ -2,20 +2,22 @@ import react from "react";
 import{ image,StyleSheet,dimensions} from "react native";
 
 import texto from "./texto";
+import topo from "../assets/assets/topo.png"
 
-export defult function topo({titulo}){
+export default function topo({titulo}){
     return<>
     <texto style={estilos.titulo}>   {titulo} </texto>
 
     </>
 }
 
-const estilos = style StyleSheet.create({
+const estilos =  StyleSheet.create({
 
     topo:{
         width: "100%",
         height:578/786 * width,
     },
+    titulo:{
         width:"100%",
         position: "absolute",
         textAlign:"center",
