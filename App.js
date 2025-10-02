bc38066c1269a4ed573bc9a7ddc49c51d2e554f8
Tiago <tiago.amancio@escola.pr.gov.br>
2025-10-02 14:12:20 +0000

    17

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import {us fonts, montserrat-400regular,montserrat_700bold} from '@expo-google-fonts/montserrat';
 
 export default function App() {
+
+  const [fonte carregada] = use fonts({
+    "mont seerratRegular":montserrat_400regular
+
+
   return (
  <SafeAreaView>
   <StatusBar />
