25d8923a3e55fb17604b535a08edde8582aa6c88
Tiago <tiago.amancio@escola.pr.gov.br>
2025-10-09 11:28:06 +0000

    24

---
+++ src/mocks/cesta.js
@@ -0,0 +1,21 @@
+import logo from '../../assets/logo.png';
+
+import tomate from '../.../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png'
+import abobora from '../../assets/frutas/Abóbora.png'
+
+const cesta ={
+    topo:{
+        titulo: "Detalhe da cesta",
+}
+detalhes:{
+    nome:"cesta de verduras",
+    logoFazenda: logo,
+    momeFazenda: "Jenny Jack Farm"
+    descricao: "uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha"
+
+
+
+}
